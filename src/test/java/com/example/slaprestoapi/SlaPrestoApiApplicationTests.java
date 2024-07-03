b346886884459597d4de1ecc621e6cf2665b2dde
nansari <nansari@twilio.com>
2024-07-03 19:32:25 +0300

    SpringBoot initializer

---
+++ src/test/java/com/example/slaprestoapi/SlaPrestoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slaprestoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlaPrestoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
